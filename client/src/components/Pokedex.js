3cc6b1eb96fd3e4dfb284b9ecf594758cdd7c21c
JosiasAssamoi <assamoi.josias@gmail.com>
2019-12-22 21:49:32 +0100

    add sorting feature

--- client/src/components/Pokedex.js
+++ client/src/components/Pokedex.js
@@ -19,11 +19,22 @@ export default class Pokedex extends Component {
         const input_type =document.getElementById('search_param').value
         
         let pokemons = this.state.initialPokemons
-
-        
        pokemons = pokemons.filter( (pokemon)=> {return pokemon[input_type].toLowerCase().includes(input) })
+        this.setState({pokemons}) 
+        
+    }
 
-       this.setState({pokemons})
+    sortList(e){
+        const input_type = e.target.value
+        let pokemons = this.state.pokemons
+        if (input_type === 'ndex'){
+            pokemons = pokemons.sort((a,b) =>  a[input_type]-b[input_type] )
+        }
+        else {
+            pokemons = pokemons.sort((a,b) => { if(a[input_type] < b[input_type]) {return -1} if(a[input_type] > b[input_type]) {return 1} return 0} )
+        }
+        
+        this.setState({pokemons})
         
     }
 
@@ -44,13 +55,13 @@ export default class Pokedex extends Component {
                 <div className="container col-lg-4 col-md-5 col-sm-6 rounded-pill bg-light mb-5">
                     <div className="col-xs-8 col-xs-offset-2 input-group">
                         <div className="search-panel">
-                            <select name="search_param" id="search_param" className="btn btn-default rounded-pill" data-toggle="dropdown">
+                            <select name="search_param" id="search_param"  onChange={(event)=>this.sortList(event)}  className="btn btn-default rounded-pill" data-toggle="dropdown" >
                                 <option value="nom">Nom</option>
                                 <option value="ndex">Numéro</option>
                                 <option value="espece">Espèce</option>
                             </select>
                         </div>
-                        <input type="text" className="form-control" placeholder="Entrez un nom ou un numéro"  onChange={(event)=>this.filterList(event)}/>
+                        <input type="text" className="form-control" placeholder="Tapez votre recherche ici"  onChange={(event)=>this.filterList(event)}/>
                     </div>
                 </div>
                 <div className="container">
