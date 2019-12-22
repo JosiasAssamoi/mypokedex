a4bd77a5e6b0d4fa7f0ea6eadd84c241ab0f330b
JosiasAssamoi <assamoi.josias@gmail.com>
2019-12-22 21:15:01 +0100

    filtering by ndex nom and species => Done =D

--- client/src/components/Pokedex.js
+++ client/src/components/Pokedex.js
@@ -3,33 +3,69 @@ import PokemonCard from './PokemonCard'
 
 export default class Pokedex extends Component {
 
-    state = {pokemons:[]}
+    state = { initialPokemons: [] , pokemons: [] }
 
-    async fetchPokemons(){
+    async fetchPokemons() {
         const response = await fetch("http://localhost:4242/api/pokemons")
-        const pokemons =  await response.json()
-        this.setState({pokemons})
- 
+        const pokemons = await response.json()
+        this.setState({ pokemons , initialPokemons:pokemons })
+
     }
-    
-    componentDidMount(){
-       this.fetchPokemons()
-       
+
+    filterList(e){
+        const input = e.target.value
+        //name ou ndex
+        // eslint-disable-next-line
+        const input_type =document.getElementById('search_param').value
+        
+        let pokemons = this.state.initialPokemons
+
+        
+       pokemons = pokemons.filter( (pokemon)=> {return pokemon[input_type].toLowerCase().includes(input) })
+
+       this.setState({pokemons})
+        
+    }
+
+    componentDidMount() {
+        this.fetchPokemons()
+
     }
 
 
     render() {
 
-        const {pokemons}= this.state
-        
+        const { pokemons } = this.state
+
 
         return (
-            <div className="row">
-                {pokemons.map(pokemon => {
-                   return  <PokemonCard key ={pokemon.ndex} details={pokemon} />
-                    
-                })}
-            </div>
+
+            <React.Fragment>
+                <div className="container col-lg-4 col-md-5 col-sm-6 rounded-pill bg-light mb-5">
+                    <div className="col-xs-8 col-xs-offset-2 input-group">
+                        <div className="search-panel">
+                            <select name="search_param" id="search_param" className="btn btn-default rounded-pill" data-toggle="dropdown">
+                                <option value="nom">Nom</option>
+                                <option value="ndex">Numéro</option>
+                                <option value="espece">Espèce</option>
+                            </select>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Entrez un nom ou un numéro"  onChange={(event)=>this.filterList(event)}/>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div className="row">
+                                {pokemons.map(pokemon => {
+                                    return <PokemonCard key={pokemon.ndex} details={pokemon} />
+
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
